5f80adf5f02b7c0f505c64c0efaf47036bbc6531
aaaaaaaaa222234 <antonkobernik17@gmail.com>
2025-07-12 12:41:43 +0200

    aaa

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 let productsGrid=document.getElementById('products-grid');
 let productsArray=[];
 let xhr =new XMLHttpRequest();
-let url = 'https://my-json-server.typicode/Mark1906/second-new/products';
+let url = 'https://my-json-server.typicode/Mark1906/second-new';
 
 xhr.open('GET',url + '/products');
 xhr.responseType='json'
