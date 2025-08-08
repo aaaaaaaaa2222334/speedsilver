d31fb53769e1e33ab757b5422618096c03a1da0e
aaaaaaaaa222234 <antonkobernik17@gmail.com>
2025-08-08 19:50:05 +0200

    asd

--- js.js
+++ js.js
@@ -19,11 +19,71 @@ xhr.onload=function(){
         <p class='product-price><b>Price: </b>${p.price}$</p>
         <p class='product-description><b>Description: </b>${p.description}$</p>
         <a href='miraz.html?id=${p.author_id}'>Seller profile</a>
-        <button>Buy</button>`;
+        <button onclick="addProductToCart('${p._id}')">Buy</button>`;
         productsGrid.append(pElem);
         });
 }
 xhr.send();
 function openCart(){
     cartProd.classList.toggle('hide');
+}
+
+let cartProd = document.getElementById('cart-products');
+
+let cart=[];
+if(localStorage.getItem('cart')){
+    cart=JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+
+function deletProductToCart(){
+    cart=[];
+    cartProd.innerHTML='car is empty';
+    localStorage.setItem("cart",'[]');
+}
+
+function addProductToCart(id){
+    let product=productsArray.find(function(p){
+        return p._id==id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function(){
+        document.getElementById('cart-button').classList.remove('active');
+    },500);
+}
+
+function drawCartProducts(){
+    if(cart.length===0) return cartProd.innerHTML='Cart is empty';
+    cartProd.innerHTML=null;
+    let sum=0;
+    cart.forEach(function(p){
+        cartProd.innerHTML+=`
+        <p><img src="${p.photo_url}"> ${p.price}pln</p>
+        <hr>`;
+        sum+=+p.price;
+    });
+    cartProd.innerHTML+=`
+    <p>suma: ${sum}Pln</p>
+    <button onclick="buyAll()">buy all</button>
+    <button onclick="deletAll()>delet all</button>`;
+}
+
+function buyAll(){
+    modal.style.display="block";
+    let sum =0;
+    orderBlock.innerHTML=null;
+
+    cart.forEach(function(p){
+        orderBlock.innerHTML+=`
+        <div class="item">
+        <img width=100px" src="${p.photo_url}">
+        <h2>${p.name}|${p.price}PLN</h2>
+        </div>`;
+        sum+=+p.price;
+    });
+    document.getElementById('price').innerHTML=sum+'PLN';
 }
