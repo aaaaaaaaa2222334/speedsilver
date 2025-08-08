ec7c80ddadd8b9887011e96e84f04c9a98ed4c63
aaaaaaaaa222234 <antonkobernik17@gmail.com>
2025-08-08 20:09:49 +0200

    ds

--- js.js
+++ js.js
@@ -16,8 +16,8 @@ xhr.onload=function(){
         pElem.innerHTML=`
         <h2 class='product-name'>${p.name}</h2>
         <img class='product-photo' src='${p.photo_url}' alt ='${p.name}'>
-        <p class='product-price><b>Price: </b>${p.price}$</p>
-        <p class='product-description><b>Description: </b>${p.description}$</p>
+        <p class='product-price><b>Price: </b>${p.price}</p>
+        <p class='product-description><b>Description: </b>${p.description}</p>
         <a href='miraz.html?id=${p.author_id}'>Seller profile</a>
         <button onclick="addProductToCart('${p._id}')">Buy</button>`;
         productsGrid.append(pElem);
