5d55b0cceb933d7ead1b0711bcf8237e4140d715
aaaaaaaaa222234 <antonkobernik17@gmail.com>
2025-09-27 12:25:51 +0200

    dww

--- admin.js
+++ admin.js
@@ -29,3 +29,64 @@ productForm.addEventListener('submit',function(event){
 })
 
 let orders=document.getElementById('admin_page_orders');
+
+let xhr =new XMLHttpRequest();
+xhr.open("GET", "https://pespatron-3881.restdb.io/rest/product");
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-apikey", "68a067b54a80e51e65677186");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.onload=function(){
+    xhr.response.forEach(function(order){
+        let orderElement = document.createElement('div');
+        orderElement.classList.add('product');
+        let statusColor='green';
+        if(order.status=='Complated'){
+            statusColor='yellow';
+        }
+        orderElement.innerHTML+=`
+        <h2>Zamovlenia ${order._id}</h2>
+        <p><b>Status:<b><span style="color:${statusColor}">${order.status}</span></p>
+        <p><b>Name:</b> ${order.name}</p>
+        <p><b>address:</b> ${order.address}</p>
+        <p><b>phone:</b> ${order.phone}</p>
+        <p><b>post Number:</b> ${order.post_number}</p>`;
+        let sum=0;
+        order.products.forEach(function(p){
+            order.Element.innerHTML+=`
+            <p><img height="50" src="${p.photo_url}"> ${p.name} | ${p.price}$</p>
+            `;
+            sum+= +p.price;
+
+        });
+        orderElement.innerHTML+=`
+        <p>SUMA: ${sum}$</p>
+        <button onclick="complete('${order._id}')">#</button>`;
+        orders.append(orderElement);
+        
+    })
+}
+
+xhr.send()
+
+function complete(id){
+    var data=JSON.stringify({
+        "status":"Complated"
+    });
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials=false;
+
+    xhr.onload=function(){
+        if(xhr.status==200){
+            location.reload();
+        }else{
+            alert('Server error. Try again later');
+        }
+    }
+      xhr.open("POST","https://pespatron-3881.restdb.io/rest/product");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "68a067b54a80e51e65677186");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.send(data);
+}
